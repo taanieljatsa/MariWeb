d554697aa19bfb4c53b74e56cdce1a706fa62aee
taanieljatsa <taanieljatsa@gmail.com>
2022-11-09 16:44:37 +0100

    commit again and again

--- modal.js
+++ modal.js
@@ -4,8 +4,28 @@ const closeModal = document.querySelector("#close-modal");
 
 openModal.addEventListener("click", () => {
   modal.showModal();
+  stopScrolling.classList.toggle("stop-scrolling");
 });
 
 closeModal.addEventListener("click", () => {
   modal.close();
+  stopScrolling.classList.toggle("stop-scrolling");
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const stopScrolling = document.querySelector("body");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  stopScrolling.classList.toggle("stop-scrolling");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    stopScrolling.classList.toggle("stop-scrolling");
+  })
+);
